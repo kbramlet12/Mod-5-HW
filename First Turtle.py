eaf361fcc03017df7254c8c322136560f7f9a281
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-11 22:48:41 -0600

    Update First Turtle.py

--- First Turtle.py
+++ First Turtle.py
@@ -6,10 +6,11 @@
 #===============================================================================
 import turtle
 wn = turtle.Screen()
-wn.bgcolor(input("What is the fill color of your Polygon? "))
+wn.bgcolor(input("What is the fill color of your Polygon? ")) #This input should go to polly.pencolor()
 
+polly.pencolor(input("What is the line color of your Polygon? ")
 polly = turtle.Turtle()
-polly.color(input("What is the Line color of your Polygon? "))
+polly.color(input("What is the Line color of your Polygon? ")) #The polly will be filled with this input using begin_fill and end_fill
 polly.pensize(5)
 
 number_of_angles = int(input("How many sides does your polygon have? "))
@@ -17,10 +18,12 @@ side_length = int(input("How long is each side? "))
 n = number_of_angles
 angle = 360 / n
 
+polly.begin_fill()               
 for i in range(0, n):
         polly.forward(side_length)
         polly.left(angle)
-
+polly.end_fill()               
+               
 wn.exitonclick()
 
  
